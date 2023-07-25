834925eb56c7cacc684176529cd132c5556f1e4b
devinda_thilina <135606514+devindathilina@users.noreply.github.com>
2023-07-25 13:29:00 -0700

    Delete PRACTICAL 2 directory

--- PRACTICAL 2/Q2.c
+++
@@ -1,9 +0,0 @@
-// Q 02
-#include<stdio.h>
-int main ()
-{
-    printf("%5d%5d%5d\n\n",2,4,8);
-    printf("%5d%5d%5d\n\n",3,9,27);
-    printf("%5d%5d%5d\n\n",4,16,64);
-    printf("%5d%5d%5d\n\n",5,25,125);
-}
